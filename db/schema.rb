538901fb7bb297366fb4e8fd91d9f7f0fc5a17db
Kenji <kenji.au9@gmail.com>
2020-07-25 18:02:09 -0600

    Changed database from SQL to PG

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_25_211444) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
